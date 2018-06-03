e329ca90b3d7bf7ccc56dcac936b6b3f6e611a72
liangzid <2273067585@qq.com>
2018-06-03 11:59:42 +0800

    Add files via upload
    model

---
+++ model/Fusion_feature.py
@@ -0,0 +1,52 @@
+'''
+New file in vsCode,windows10 X64,pytorch,python3.
+(please check it if using it on linux)
+build the fusion part of NN
+liangzia,2018,5,23
+'''
+import torch as t
+import numpy as np
+from BasicModule import BasicModule
+from torch import nn
+
+class Fusion_feature(BasicModule):
+    def __init__(self):
+        
+        super(Fusion_feature,self).__init__()
+
+        self.model_name='FusionFeature'
+
+        self.ffusion=t.nn.Sequential(
+            nn.Linear(8192,4096,bias=1),
+            nn.Linear(4096,4096,bias=1),
+            nn.ReLU(),
+            nn.Dropout(0.5)
+
+        )
+
+    def view_as(self,feature):
+        return feature.view(feature.size(0),-1)
+
+    def cat(self,feature1,feature2):
+        feature=t.cat((feature1,feature2),0)
+        return feature
+
+    def forward(self,feature):
+        feature=self.ffusion(feature)
+        return feature
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ model/Metric_learning/XQDA.py
@@ -0,0 +1,80 @@
+# this file is learning to write a XQDA function
+# transform it from MATLAB
+# liangzi  2017 3.29
+
+import numpy
+import mpmath
+
+def XQDA(galx, probx, gal_labels, prob_labels, learn_rate=0.001,qda_dims = -1):
+
+    num_gals= galx.shape[0]
+    d=galx.shape[1]
+    num_probs=probx.shape[1]
+
+    if d>num_gals+num_probs :
+        W,X =numpy.linalg.qr(numpy.hstack((galx.T,probx.T)))
+        galx=X[:,0:num_gals-1]
+        probx=X[:,num_gals:-1]
+        d=X.shape[0]
+
+    labels=numpy.unique(numpy.vstack((gal_labels,prob_labels)))
+    c=len(labels)
+
+    galW=numpy.zeros((num_gals,1))
+    gal_class_sum=numpy.zeros((c,d))
+    probW=numpy.zeros((num_probs,1))
+    prob_class_sum=numpy.zeros((c,d))
+    ni=0
+    for k in range(c):
+        gal_index = numpy.where(gal_labels==labels(k))
+        nk=len(gal_index)
+        gal_class_sum[k-1,:]=numpy.sum(galx[gal_index-1,:],1)
+
+        prob_index = numpy.where(prob_labels == labels(k))
+        mk = len(prob_index)
+        prob_class_sum[k - 1, :] = numpy.sum(probx[prob_index - 1, :], 1)
+
+        ni += nk*mk
+        galW[gal_index-1]=mpmath.sqrt(mk)
+        probW[prob_index-1]=mpmath.sqrt(nk)
+
+    gal_sum=numpy.sum(gal_class_sum,1)
+    prob_sum=numpy.sum(prob_class_sum,1)
+    gal_cov=galx.T.dot(galx)
+    prob_cov=probx.T.dot(probx)
+
+    galx=numpy.einsum('ij,jk',galW,galx)  # maybe wrong
+    probx=numpy.einsum('i,i',probW,probx)
+
+    in_cov=galx.T.dot(galx)+probx.T.dot(probx)-gal_class_sum.T.dot(prob_class_sum)-prob_class_sum.T.dot(gal_class_sum)
+    ex_cov=num_probs.dot(gal_cov)+num_gals.dot(prob_cov)-gal_sum.T.dot(prob_sum)-prob_sum.T.dot(gal_sum)-in_cov
+
+    ne=num_gals.dot(num_probs)-ni
+    in_cov /=ni
+    ex_cov /=ne
+
+    in_cov += learn_rate*numpy.eye(d)
+    V,S,u = numpy.linalg.svd(numpy.linalg.inv(in_cov).dot(ex_cov))
+    latent=numpy.diag(S)
+
+    # maybe??
+    index=numpy.argsort(-latent)
+    latent=numpy.sort(-latent)
+
+    energy=numpy.sum(latent)
+    minv=latent[-1]
+    r=numpy.sum(latent>1)
+    energy=numpy.sum(latent[0:r-1])/energy
+
+    if qda_dims>r :
+        qda_dims=max(1,r)
+
+    V=V[:,index[0:qda_dims-1]]
+
+
+
+    in_cov=V.T.dot(in_cov).V
+    ex_cov=V.T.dot(ex_cov).V
+    M=numpy.linalg.inv(in_cov)-numpy.linalg.inv(ex_cov)
+
+    return M
